168007816a12bd3fe716535c15c0818d700b8a5e
luismm2006 <luismulamarquez@gmail.com>
2025-02-25 23:56:53 +0100

    Realizacion de test, fallos en los test de objeto con mapas.

--- test/testContarPalabra/ContarPalabraObjtest.java
+++ test/testContarPalabra/ContarPalabraObjtest.java
@@ -9,9 +9,36 @@ import contarPalabra.ContarPalabra;
 class ContarPalabraObjtest {
 	
 	@Test
+    public void contarPalabrasTest() {
+        ContarPalabra str = new ContarPalabra("Hola Mundo");
+        assertEquals(2, str.getContarPalabras());
+    }
+    
+    @Test
+    public void contarLetrasDiferentesTest() {
+        ContarPalabra str = new ContarPalabra("Hola Mundo");
+        assertEquals(8, str.getContarLetrasDiferentes());
+    }
+    
+    @Test
+    public void contarFrecuenciaPalabrasTest() {
+        ContarPalabra str = new ContarPalabra("Hola mundo hola");
+        assertEquals(2, str.getContarFrecuenciaPalabras());
+        assertEquals(1, str.getContarFrecuenciaPalabras());
+    }
+    
+    @Test
+    public void contarFrecuenciaLetrasTest() {
+        ContarPalabra str = new ContarPalabra("Hola");
+        assertEquals(1, str.getContarFrecuenciaLetras());
+        assertEquals(2, str.getContarFrecuenciaLetras());
+        assertEquals(1, str.getContarFrecuenciaLetras());
+        assertEquals(1, str.getContarFrecuenciaLetras());
+    }
+	@Test
 	public void esPalindromaTest() {
 		ContarPalabra str = new ContarPalabra("Hola Mundo!");
 		assertFalse(str.getEsPalindroma());
 	}
-
+	
 }
